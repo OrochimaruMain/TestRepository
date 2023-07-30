0d975c47359d1e0bd1a65dee2d00438f040f107d
Cream123 <ryusei.ykei55@gmail.com>
2023-07-30 13:18:18 +0900

    初回コミット

---
+++ src/main/java/com/example/demo/SampleBootApp20230730Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleBootApp20230730Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleBootApp20230730Application.class, args);
+	}
+
+}
